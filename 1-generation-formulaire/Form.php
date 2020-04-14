626854150934a8914223c06b0c3bbe9fbfc1e301
ludovic <ludovic.angenard@orange.fr>
2020-04-14 15:52:43 +0200

    correction guillement

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -5,11 +5,11 @@ class Form{
 
     public  function __construct(string $action, string $method){ // le construct permet de créer la balise ouvrante <form> à partir de l'action et de la méthode
         $this->html="<form action= $action  method= $method >";
-     return $this->html;
+        return $this->html;
     }
 
-    public function addTextField(string $name, string $lastname){ // permet de créer un input, le nom et la valeur de celui ci sont requis
-       $this->html.= "<input name=$name value=$lastname >";
+    public function addTextField(string $name, string $value){ // permet de créer un input, le nom et la valeur de celui ci sont requis
+       $this->html.= "<input name='$name' value='$value' >";
        return $this;
     }
 
@@ -18,25 +18,26 @@ class Form{
         return $this;
     }
 
-    public function addSelect(string $name,string $options){ // permet de créer un select a partir du name et d'un tableau 
+    public function addSelect(string $name,array $options){ // permet de créer un select a partir du name et d'un tableau 
         $this->html.="<select name=$name >";
         foreach($options as $nb => $option){
-            $this->html.="<option value=$name.$option.$nb>$option</option>";
+            $this->html.="<option value='$nb'>$option</option>";
         }
         
         $this->html.="</select>";
     }
 
     public function addTextArea(string $name,string $text){ // permet de créer un textarea a partir du name et de son contenu 
-        $this->html.=" <textarea  name=$name > $text</textarea>";
+        $this->html.=" <textarea  name='$name' > $text</textarea>";
     }
 
-    public function addRadio(string $name, string $radios){ // permet de créer des radio bouton a partir du name et d'un tableau 
+    public function addRadio(string $name, array $radios){ // permet de créer des radio bouton a partir du name et d'un tableau 
         foreach($radios as $nb => $radio){
-            $this->html.="<div> <input type=radio  name=$name value=$name.$nb.$radio> <label for= $name.$nb.$radio> $radio</label> </div>";
+            $this->html.="<div> <input type=radio  name='$name'' value='$nb'> <label for='$nb'> $radio</label> </div>";
         }
        
     }
+
     public function build(){ // permet de fermer la balise <form> et de retourner le formulaire html
         return "$this->html </form>";
     }
