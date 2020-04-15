6df439972c927355f2a05a4c69af48a8b9be2a2f
Erwann Duclos <erwann.duclos@gmail.com>
2020-04-15 11:19:32 +0200

    Update HTMLField.php

--- 2-refactoring-de-classe/HtmlField.php
+++ 2-refactoring-de-classe/HtmlField.php
@@ -3,12 +3,12 @@ abstract class HtmlField {
     private $name;
     private $value;
 
-    private function isValid(){
+    private function isValid($value){
         return true;
     }
 
     public function __construct($name, $value) {
-        if ($this->isValid()) { 
+        if ($this->isValid($value)) { 
             $this->name= $name;
             $this->value = $value;
         }
